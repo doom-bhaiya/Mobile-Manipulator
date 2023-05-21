047983d0fb5c6dcfce904c5be6604f1a279f8a76
rubankumard <drubankumar1202@gmail.com>
2023-05-21 17:23:28 +0530

    Script for navigation

--- vehicle_environment/scripts/move.py
+++ vehicle_environment/scripts/move.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
-
+'''
+This is a demo script for testing linear motion of the bot by moving all four wheels in same speed.'''
 import rospy
 from std_msgs.msg import Float64
-import time 
-rospy.init_node("controller1")
-controller1 = rospy.Publisher("position_controller1/command", Float64, queue_size = 10)
-test = Float64(-100)
-for i in range(1):
-    controller1.publish(test)
-    time.sleep(0.1)
-    rospy.loginfo("Running")
+import time
 
-time.sleep(10)
+speed = 1
+
+def straight(vel):
+    global wheel_pub
+    while True:
+        msg = Float64()
+        msg.data = vel
+        for pub in wheel_pub:
+            pub.publish(msg)
+        rospy.sleep(0.01)
+        print (msg)
+
+
+def main():
+    global wheel_pub
+    wheel_pub = [0,0,0,0]
+    rospy.init_node("wheel_controller")
+    wheel_pub[0] = rospy.Publisher('/velocity_controller1/command', Float64, queue_size=10) #frontside_left_wheel
+    wheel_pub[1] = rospy.Publisher('/velocity_controller2/command', Float64, queue_size=10) #backside_left_wheel
+    wheel_pub[2] = rospy.Publisher('/velocity_controller3/command', Float64, queue_size=10) #frontside_right_wheel
+    wheel_pub[3] = rospy.Publisher('/velocity_controller4/command', Float64, queue_size=10) #backside_right_wheel
+    straight(speed)
+main()
+if (__name__ == '__main__'):
+    try:
+        pass
+    except :
+        rospy.loginfo("Script Terminated!")
+
 # test = Float64(0)
 # controller1.publish(test)
---
+++ vehicle_environment/scripts/wheel_control.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+'''
+This python script is used to drive the four wheeled bot under the control of /cmd_vel
+
+Publish linear.x velocity for linear motion and angular.z velocity for rotational motion in /cmd_vel topic, which is of type Twist.
+'''
+import rospy
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+
+rospy.init_node('robot_wheel_controller')
+
+# Define the individual wheel control topics
+wheel1_topic = '/velocity_controller1/command'
+wheel2_topic = '/velocity_controller2/command'
+wheel3_topic = '/velocity_controller3/command'
+wheel4_topic = '/velocity_controller4/command'
+
+# Create publisher objects for the individual wheel control topics
+wheel1_pub = rospy.Publisher(wheel1_topic, Float64, queue_size=10)
+wheel2_pub = rospy.Publisher(wheel2_topic, Float64, queue_size=10)
+wheel3_pub = rospy.Publisher(wheel3_topic, Float64, queue_size=10)
+wheel4_pub = rospy.Publisher(wheel4_topic, Float64, queue_size=10)
+
+# Robot width (Distance between two wheels of the same side in meters)
+robot_width = 0
+
+# Callback function for cmd_vel messages
+def cmd_vel_callback(msg):
+    # Extract linear and angular velocities from the received Twist message
+    linear_velocity = msg.linear.x
+    angular_velocity = msg.angular.z
+
+    # Calculate individual wheel speeds based on the desired motion
+    left_wheel_speed = linear_velocity - (angular_velocity * robot_width / 2)
+    right_wheel_speed = linear_velocity + (angular_velocity * robot_width / 2)
+
+    # Publish the calculated wheel speeds to the individual wheel control topics
+    wheel1_pub.publish(left_wheel_speed)
+    wheel2_pub.publish(left_wheel_speed)
+    wheel3_pub.publish(right_wheel_speed)
+    wheel4_pub.publish(right_wheel_speed)
+
+# Register shutdown callback function
+def shutdown_callback():
+    # Stop the bot by publishing zero wheel speeds
+    wheel1_pub.publish(0.0)
+    wheel2_pub.publish(0.0)
+    wheel3_pub.publish(0.0)
+    wheel4_pub.publish(0.0)
+    rospy.loginfo('\033[91m' +"Bot stopped."+ '\033[0m')
+
+rospy.on_shutdown(shutdown_callback)
+
+# Subscribe to the cmd_vel topic
+rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
+
+rospy.loginfo('\033[92m' + "Wheel_velocity_controller_Started" + '\033[0m')
+
+rospy.spin()  # Keeps the program running until it is explicitly stopped
